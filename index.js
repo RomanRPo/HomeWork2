1e55f1d0866a72806242c375d448df21042f04d7
Roman <romapopletuhin@gmail.com>
2021-03-20 18:57:29 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var person = 'RomanPoplietukhin';
+var age = 27;
+var greating = 'hello';
+
+info = 'RomanPoplietukhin' + '' + age + '' + 'greating';
+
+console.log('info');
+
+//+'40' + +'2' => 42;
+//'2' + 3 ** 2 => 29;
+//3 ** 2 / 3 => 3;
+//3 ** (9 / 3) => 27;
+//undefined + 1 => 
+//+null + 10 =>
+//null / 5 =>
+//'hello' + null =>
+//'hello' - undefined + ' ' =>
+//'30' + 3 - '3' => 300;
+//10 % '5' => 0;
+
+//var number = 5;
+//2 * ++number; 
+//2 * number--; 
+//console.log(number);
